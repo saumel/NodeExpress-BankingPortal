dec0438ddf1232296daf959d87ac7e8d15101bd7
Saumel <saumelcaro@outlook.com>
2020-05-01 20:31:24 -0400

    first

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
+const express = require('express');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine','ejs');
+
+
